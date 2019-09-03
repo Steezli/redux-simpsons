073f9e6fb140bc2bbb47e41010caa75b81055287
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-09-02 21:26:21 -0700

    initial reducer

---
+++ src/reducers/simpsons.js
@@ -0,0 +1,15 @@
+
+
+
+const initialState = {
+  loading: false,
+  error: null,
+  ist: []
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
